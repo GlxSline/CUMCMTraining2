3613d9d8aebd60e6676d79f4bbd26a681816912b
GlxSline <glx15424872@163.com>
2025-07-01 09:50:16 +0800

    Q1-1

---
+++ Codes/Question1-1/data.m
@@ -0,0 +1,11 @@
+theta_last = (1:8) .* 2 .*pi /9;
+% theta_now = [0, 40.1, 80.21, 119.75, 159.86, 199.96, 240.07, 280.17, 320.80] ./ 360 .* 2 .* pi;
+theta_now = [40.1, 80.21, 119.75, 159.86, 199.96, 240.07, 280.17, 320.80] ./ 360 .* 2 .* pi;
+rho_last = 100 .* linspace(1,1,8);
+% rho_now = [100, 98, 112, 105, 98, 112, 105, 98, 112];
+rho_now = [98, 112, 105, 98, 112, 105, 98, 112];
+
+x_last = rho_last .* cos(theta_last);
+y_last = rho_last .* sin(theta_last);
+x_now  = rho_now  .* cos(theta_now);
+y_now  = rho_now  .* sin(theta_now);
